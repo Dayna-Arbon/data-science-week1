992cfd2c4d101e160677db46c800055dba7b9aed
Dayna-Arbon <rbd24qzu@uea.ac.uk>
2026-02-06 17:25:36 +0000

    Adding cleaning steps to raw data

--- scripts/initial_exploration.R
+++ scripts/initial_exploration.R
@@ -37,6 +37,76 @@ mosquito_egg_raw |>
 # - How many observations?
 #   205
 # - Anything surprising?
-#   there is a -93.0 body mass
+#   there is a -93.0 body mass, there are lower case and upper case letters
 # - Any obvious problems?
-#   There are 13 missing values in the collector column 
+#   There are 13 missing values in the collector column, looks like id 13 is duplicated 
+
+# identifying missing data ----
+naniar::gg_miss_upset(mosquito_egg_raw)
+
+# checking for duplicates ----
+
+mosquito_egg_raw |> 
+  get_dupes()
+
+# 4 duplicates. 
+
+mosquito_egg_raw |> mutate(collection_date = as_date(collection_date))
+# no error so dates are ok. 
+
+
+# FIX 1: upper and lower cases ====
+
+# Show the problem:
+mosquito_egg_raw |> distinct(treatment)
+
+
+# Fix it: 
+# YOUR CODE HERE
+mosquito_egg_data_step1 <- mosquito_egg_raw |>
+  mutate(treatment = case_when(
+    treatment == "Medium_dose" ~ "medium_dose",
+    treatment == "High_dose" ~ "high_dose",
+    treatment == "Control" ~ "control",
+    treatment == "HIGH_DOSE" ~ "high_dose",
+    treatment == "LOW_DOSE" ~ "low_dose",
+    treatment == "MEDIUM_DOSE" ~ "medium_dose",
+    treatment == "CONTROL" ~ "control",
+    treatment == "Low_dose" ~ "low_dose",
+    .default = as.character(treatment)))
+  
+# Verify it worked:
+mosquito_egg_data_step1 |> distinct(treatment)
+  
+  
+  # What changed and why it matters:
+  # all types of treatment changed to same name - all lower case- so when inputting code r doesnt miss any values.
+  # this makes the process of analysing the data in r a much smoother process.
+  
+  
+  # FIX 2:  removing negative and impossible body_mass_mg values ====
+
+# Show the problem:
+mosquito_egg_raw |> skim(body_mass_mg)                   
+
+# YOUR CODE
+# Fix it:
+mosquito_egg_data_step2 <- filter(mosquito_egg_data_step1, body_mass_mg > 0)
+  
+
+  # Verify it worked:
+mosquito_egg_data_step2 |> skim(body_mass_mg) 
+  
+  # What changed and why it matters:
+  # the values in the negative are impossible.  
+  # therefore averages/ means of the data will be inaccurate 
+
+
+
+
+
+
+
+
+
+
